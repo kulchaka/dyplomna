26ae2a252c5082dd7e499bfdc23a978372325eed
Andrii <kulchaka@gmail.com>
2020-09-26 14:28:08 +0200

    Stage-3 JS

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,8 @@ const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
     entry: {
         index: './src/js/index.js',
-        about: './src/js/about.js',
-        analytics: './src/js/analytics.js'
+        about: './src/js/about/about.js',
+        analytics: './src/js/analytics/analytics.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
